4c2db3be2973e1fb28764a343f134f36ebc3abd4
AIFanatic <9558106+AIFanatic@users.noreply.github.com>
2024-11-29 06:39:52 -0100

    InstancedMeshes now uses DynamicBufferAllocator

--- src/components/InstancedMesh.ts
+++ src/components/InstancedMesh.ts
@@ -1,25 +1,22 @@
 import { Buffer, BufferType } from "../renderer/Buffer";
 import { Matrix4 } from "../math/Matrix4";
 import { Mesh } from "./Mesh";
+import { DynamicBufferMemoryAllocator } from "../utils/MemoryAllocator";
 
 export class InstancedMesh extends Mesh {
-    private _maxInstanceCount = 1000000;
-    private _matricesBuffer: Buffer = Buffer.Create(this.maxInstanceCount * 4 * 16, BufferType.STORAGE);
-    public get matricesBuffer(): Buffer { return this._matricesBuffer }
+    private incrementInstanceCount = 1000;
+    private _matricesBuffer = new DynamicBufferMemoryAllocator(this.incrementInstanceCount * 16);
     
-    private get maxInstanceCount(): number { return this._maxInstanceCount; }
-    private set maxInstanceCount(maxInstanceCount: number) {
-        this._maxInstanceCount = maxInstanceCount;
-        Buffer.Create(this.maxInstanceCount * 4 * 16, BufferType.STORAGE);
-    }
+    public get matricesBuffer(): Buffer { return this._matricesBuffer.getBuffer() }
 
     private _instanceCount: number = 0;
     public get instanceCount(): number { return this._instanceCount};
 
     public SetMatrixAt(index: number, matrix: Matrix4) {
         if (!this._matricesBuffer) throw Error("Matrices buffer not created.");
-        if (index > this.maxInstanceCount) throw Error("Trying to create more instances than max instance count.");
+        // if (index > this.maxInstanceCount) throw Error("Trying to create more instances than max instance count.");
         this._instanceCount = Math.max(index, this._instanceCount);
-        this._matricesBuffer.SetArray(matrix.elements, 4 * 16 * index);
+        // this._matricesBuffer.SetArray(matrix.elements, 4 * 16 * index);
+        this._matricesBuffer.set(index, matrix.elements);
     }
 }
