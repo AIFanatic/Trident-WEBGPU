4c48b2be1c1f297daf1fc2de92057e7c37155ec8
AIFanatic <9558106+AIFanatic@users.noreply.github.com>
2025-11-09 17:04:36 -0100

    initial

---
+++ packages/plugins/PhysicsRapier/ThirdPersonController.ts
@@ -0,0 +1,150 @@
+import { Component, Components, Mathf, GPU, GameObject, Input, KeyCodes } from "@trident/core";
+import { PhysicsRapier } from "./PhysicsRapier";
+import { RigidBody } from "./RigidBody";
+
+enum State {
+    Idle,
+    Walking,
+    Running,
+    Jumping,
+    Falling
+};
+
+export class ThirdPersonController extends Component {
+    public _controller: RigidBody;
+    public _animator: Components.Animator;
+    public _model: GameObject;
+    public _mainCamera: GameObject;
+    public _animationIDS: { idle: number, walk: number, sprint: number, jump: number, fall: number };
+
+    private move = new Mathf.Vector2();
+    private look = new Mathf.Vector2();
+    private jump = false;
+    private sprint = false;
+    private isPointerLocked = false;
+    private isGrounded = false;
+    private currentAnimation: number = -1;
+
+    private v = new Mathf.Vector3();
+
+    public speed = 5;
+    public boostMultiplier = 10;
+
+    public orbitSpeed = 0.01;
+    public rayDistance = 1;
+    public playHeight = 1.80;
+    public blendRotation = 0.15;
+
+    private state: State = State.Walking;
+
+    public Start() {
+        if (!this._controller) throw Error("No controller attached");
+        if (!this._animator) console.warn("No animator attached");
+        if (!this._model) throw Error("No model attached");
+        if (!this._mainCamera) throw Error("No camera attached");
+        if (!this._animationIDS) throw Error("No animation ids set");
+
+        GPU.Renderer.canvas.addEventListener("pointerdown", event => {
+            if (!this.isPointerLocked) document.body.requestPointerLock();
+        })
+
+        document.onpointerlockchange = event => {
+            this.isPointerLocked = !this.isPointerLocked;
+        }
+    }
+
+    public Update() {
+        this.UpdateInput();
+        this.GroundedCheck();
+        this.Move();
+        this.CameraRotation();
+        this.UpdateState();
+        this.UpdateAnimation();
+    }
+
+    private UpdateState() {
+        const isGrounded = this.isGrounded;
+        const isJumping = this.jump;
+        const isMoving = this.move.length() > 1e-5;
+        const isRunning = this.sprint;
+
+        if (isGrounded === true && !isMoving) this.state = State.Idle;
+        else if (isGrounded === true && isMoving) isRunning ? this.state = State.Running : this.state = State.Walking;
+        else if (isGrounded === true && isMoving && isRunning) this.state = State.Running;
+        else if (isJumping === true) this.state = State.Jumping;
+    }
+
+    private UpdateAnimation() {
+        let currentAnimation = -1;
+        if (this.state === State.Idle) currentAnimation = this._animationIDS.idle;
+        if (this.state === State.Walking) currentAnimation = this._animationIDS.walk;
+        if (this.state === State.Running) currentAnimation = this._animationIDS.sprint;
+        if (this.state === State.Jumping) currentAnimation = this._animationIDS.jump;
+
+        if (currentAnimation !== -1 && this.currentAnimation !== currentAnimation) this._animator.CrossFadeTo(currentAnimation);
+
+        this.currentAnimation = currentAnimation;
+    }
+    
+    private UpdateInput() {
+        this.move.x = (Input.GetKey(KeyCodes.D) ? 1 : 0) - (Input.GetKey(KeyCodes.A) ? 1 : 0);
+        this.move.y = (Input.GetKey(KeyCodes.W) ? 1 : 0) - (Input.GetKey(KeyCodes.S) ? 1 : 0);
+        this.jump = Input.GetKey(KeyCodes.SPACE);
+        this.sprint = Input.GetKey(KeyCodes.SHIFT);
+        this.look.x = -Input.GetAxis("Horizontal");
+        this.look.y = -Input.GetAxis("Vertical");
+    }
+    private GroundedCheck() {
+        const rigidbody = this._controller.rigidBody;
+        const p = rigidbody.translation();
+        const ray = new PhysicsRapier.Physics.Ray(p, { x: 0, y: -1, z: 0 });
+        const rayHit = PhysicsRapier.PhysicsWorld.castRay(ray, this.rayDistance + 2, true, undefined, undefined, undefined, rigidbody);
+
+        this.isGrounded = rayHit && rayHit.collider && Math.abs(rayHit.timeOfImpact) <= 1.75
+    }
+
+    private Move() {
+        this.v.set(0, 0, 0);
+        this.v.set(this.move.x, 0, -this.move.y);
+
+        this.v.applyQuaternion(this._mainCamera.transform.rotation);
+
+        const rigidbody = this._controller.rigidBody;
+        const velocity = rigidbody.linvel()
+        const direction = this.v.mul(this.sprint ? this.boostMultiplier : this.speed);
+        rigidbody.setLinvel({ x: direction.x, y: velocity.y, z: direction.z }, true);
+
+        // jumping
+        if (this.jump && this.isGrounded) rigidbody.setLinvel({ x: 0, y: 7.5, z: 0 }, true);
+
+        const p = rigidbody.translation();
+        this._mainCamera.transform.position.set(p.x, p.y, p.z).add(new Mathf.Vector3(0, -1.5 + this.playHeight, 0)).sub(new Mathf.Vector3(0, 0, -2));
+        this._model.transform.position.set(p.x, p.y - 1.5, p.z);
+
+        // keep/store model rotation
+        if (Math.abs(this.move.x) > Mathf.Epsilon || Math.abs(this.move.y) > Mathf.Epsilon) {
+            const targetRotation = (Mathf.Atan2(-this.move.x, this.move.y) + this.yaw) * Mathf.Rad2Deg;
+            this.currentRotation = Mathf.Lerp(this.currentRotation, targetRotation, this.blendRotation);
+            this._model.transform.rotation.fromEuler(new Mathf.Vector3(0, this.currentRotation, 0), true);
+        }
+    }
+    private currentRotation = 0;
+
+    private yaw = 0;
+    private pitch = 0;
+
+    private CameraRotation() {
+        const minPhi = -Math.PI / 2;
+        const maxPhi = 0;
+        const distance = this._mainCamera.transform.position.distanceTo(this.transform.position);
+
+        if (this.isPointerLocked) {
+            this.yaw += this.look.x * this.orbitSpeed;
+            this.pitch += this.look.y * this.orbitSpeed;
+            this.pitch = Math.min(maxPhi, Math.max(minPhi, this.pitch));
+        }
+
+        this._mainCamera.transform.rotation.fromEuler(new Mathf.Vector3(this.pitch, this.yaw, 0));
+        this._mainCamera.transform.position.set(0, 0, distance).applyQuaternion(this._mainCamera.transform.rotation).add(this.transform.position);
+    }
+}
