ead1597cb2ddcf93efbcc552a785b15c801ec8e3
AIFanatic <9558106+AIFanatic@users.noreply.github.com>
2026-06-04 11:17:18 +0000

    Better normal generation

--- packages/core/Geometry.ts
+++ packages/core/Geometry.ts
@@ -199,41 +199,54 @@ export class Geometry {
     }
 
     public ComputeNormals() {
-        let posAttrData = this.attributes.get("position")?.array;
-        let indexAttrData = this.index?.array;
+        const posAttrData = this.attributes.get("position")?.array;
+        const indexAttrData = this.index?.array;
         if (!posAttrData || !indexAttrData) throw Error("Cannot compute normals without vertices and indices");
 
-        let normalAttrData = new Float32Array(posAttrData.length);
+        const normalAttrData = new Float32Array(posAttrData.length);
 
-        let trianglesCount = indexAttrData.length / 3;
-        let point1 = new Vector3(0, 1, 0);
-        let point2 = new Vector3(0, 1, 0);
-        let point3 = new Vector3(0, 1, 0);
-        let crossA = new Vector3(0, 1, 0);
-        let crossB = new Vector3(0, 1, 0);
+        const trianglesCount = indexAttrData.length / 3;
+        const point1 = new Vector3();
+        const point2 = new Vector3();
+        const point3 = new Vector3();
+        const edge1 = new Vector3();
+        const edge2 = new Vector3();
+        const normal = new Vector3();
 
         for (let i = 0; i < trianglesCount; i++) {
-            let index1 = indexAttrData[i * 3];
-            let index2 = indexAttrData[i * 3 + 1];
-            let index3 = indexAttrData[i * 3 + 2];
+            const index1 = indexAttrData[i * 3];
+            const index2 = indexAttrData[i * 3 + 1];
+            const index3 = indexAttrData[i * 3 + 2];
 
             point1.set(posAttrData[index1 * 3], posAttrData[index1 * 3 + 1], posAttrData[index1 * 3 + 2]);
             point2.set(posAttrData[index2 * 3], posAttrData[index2 * 3 + 1], posAttrData[index2 * 3 + 2]);
             point3.set(posAttrData[index3 * 3], posAttrData[index3 * 3 + 1], posAttrData[index3 * 3 + 2]);
 
-            crossA.copy(point1).sub(point2).normalize();
-            crossB.copy(point1).sub(point3).normalize();
+            edge1.copy(point2).sub(point1);
+            edge2.copy(point3).sub(point1);
+            normal.copy(edge1).cross(edge2);
 
-            let normal = crossA.clone().cross(crossB).normalize();
+            normalAttrData[index1 * 3] += normal.x;
+            normalAttrData[index1 * 3 + 1] += normal.y;
+            normalAttrData[index1 * 3 + 2] += normal.z;
 
-            normalAttrData[index1 * 3] = normalAttrData[index2 * 3] = normalAttrData[index3 * 3] = normal.x;
-            normalAttrData[index1 * 3 + 1] = normalAttrData[index2 * 3 + 1] = normalAttrData[index3 * 3 + 1] = normal.y;
-            normalAttrData[index1 * 3 + 2] = normalAttrData[index2 * 3 + 2] = normalAttrData[index3 * 3 + 2] = normal.z;
+            normalAttrData[index2 * 3] += normal.x;
+            normalAttrData[index2 * 3 + 1] += normal.y;
+            normalAttrData[index2 * 3 + 2] += normal.z;
+
+            normalAttrData[index3 * 3] += normal.x;
+            normalAttrData[index3 * 3 + 1] += normal.y;
+            normalAttrData[index3 * 3 + 2] += normal.z;
         }
 
-        let normals = this.attributes.get("normal");
-        if (!normals) normals = new VertexAttribute(normalAttrData);
-        this.attributes.set("normal", normals);
+        for (let i = 0; i < normalAttrData.length; i += 3) {
+            normal.set(normalAttrData[i], normalAttrData[i + 1], normalAttrData[i + 2]).normalize();
+            normalAttrData[i] = normal.x;
+            normalAttrData[i + 1] = normal.y;
+            normalAttrData[i + 2] = normal.z;
+        }
+
+        this.attributes.set("normal", new VertexAttribute(normalAttrData));
     }
 
     // From THREE.js (adapted/fixed)
