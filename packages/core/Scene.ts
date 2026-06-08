aadf3c74e0236e836ce9ffd2984cdcf2b9400347
AIFanatic <9558106+AIFanatic@users.noreply.github.com>
2026-06-08 06:23:27 +0000

    Better lifecycle management

--- packages/core/Scene.ts
+++ packages/core/Scene.ts
@@ -83,6 +83,7 @@ export class Scene {
                     try { c.Start(); } // Dont crash everything
                     catch (err) { console.error(`[${c.constructor.name}.Start]`, err); c.enabled = false; }
                 }
+                if (!c.shouldUpdate) continue; // TODO: Empty updates, this can be optimized further by tracking on a list
                 try { c.Update(); } // Dont crash everything
                 catch (err) { console.error(`[${c.constructor.name}.Update]`, err); c.enabled = false; }
             }
