1fdb7735b27c013dc144e440afe584c38d68be0f
AIFanatic <9558106+AIFanatic@users.noreply.github.com>
2026-07-07 08:13:54 +0000

    Revert prefab

--- packages/core/Prefab.ts
+++ packages/core/Prefab.ts
@@ -5,12 +5,29 @@ export class Prefab {
     public static type = "@trident/core/Prefab";
 
     @SerializeField public assetPath?: string;
+
+    public id?: string;
+    public name: string;
+    public components: any[] = [];
+    public transform: any;
+    public children: Prefab[] = [];
     public data: any;
 
+    public traverse(fn: (prefab: Prefab) => void) {
+        fn(this);
+        for (const child of this.children) child.traverse(fn);
+    }
+
     public static Deserialize(assetPath: string, data: any, asset: any): Prefab {
+        const source = asset ?? data;
         const prefab = new Prefab();
-        prefab.assetPath = assetPath || undefined;
-        prefab.data = asset ?? data;
+        prefab.id = source.id;
+        prefab.name = source.name;
+        prefab.assetPath = assetPath;
+        prefab.transform = source.transform;
+        prefab.components = Array.isArray(source?.components) ? source.components : [];
+        prefab.children = Array.isArray(source?.children) ? source.children.map((c: any) => Prefab.Deserialize(c.assetPath, null, c)) : [];
+        prefab.data = source;
         return prefab;
     }
 }
