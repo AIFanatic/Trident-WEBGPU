f36909d642ed61f63649db64b097f684bc2739a8
AIFanatic <9558106+AIFanatic@users.noreply.github.com>
2026-08-15 08:12:21 +0000

    No more shadowInstancedMeshes

--- packages/core/renderer/passes/SceneExtractPass.ts
+++ packages/core/renderer/passes/SceneExtractPass.ts
@@ -1,26 +1,15 @@
 import { Camera } from "../../components/Camera";
 import { Light } from "../../components/Light";
 import { Renderable } from "../../components/Renderable";
-import { Buffer } from "../Buffer";
 import { RenderPass, ResourcePool } from "../RenderGraph";
 import { PassParams } from "../RenderingPipeline";
-
-export type InstancedRenderable = Renderable & {
-    matricesBuffer: Buffer;
-    instanceCount: number;
-};
 
 export interface FrameRenderData {
     deferredRenderables: Renderable[];
     forwardRenderables: Renderable[];
     shadowCasters: Renderable[];
-    shadowInstancedMeshes: InstancedRenderable[];
     lights: Light[];
 }
-
-const isInstancedRenderable = (renderable: Renderable): renderable is InstancedRenderable => {
-    return (renderable as any).matricesBuffer !== undefined && (renderable as any).instanceCount !== undefined;
-};
 
 export class SceneExtractPass extends RenderPass {
     public name: string = "SceneExtractPass";
@@ -39,7 +28,6 @@ export class SceneExtractPass extends RenderPass {
         const deferredRenderables: Renderable[] = [];
         const forwardRenderables: Renderable[] = [];
         const shadowCasters: Renderable[] = [];
-        const shadowInstancedMeshes: InstancedRenderable[] = [];
 
         for (const [, renderable] of Renderable.Renderables) {
             if (renderable.gameObject.scene !== scene) continue;
@@ -50,20 +38,13 @@ export class SceneExtractPass extends RenderPass {
             if (renderable.material.params.isDeferred === true) deferredRenderables.push(renderable);
             else forwardRenderables.push(renderable);
 
-            if (renderable.enableShadows) {
-                if (isInstancedRenderable(renderable)) {
-                    if (renderable.instanceCount > 0) shadowInstancedMeshes.push(renderable);
-                } else {
-                    shadowCasters.push(renderable);
-                }
-            }
+            if (renderable.enableShadows) shadowCasters.push(renderable);
         }
 
         const frameData: FrameRenderData = {
             deferredRenderables,
             forwardRenderables,
             shadowCasters,
-            shadowInstancedMeshes,
             lights,
         };
 
